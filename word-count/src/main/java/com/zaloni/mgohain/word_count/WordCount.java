57774702bf3d1919fb5881cf33fef4522445edc9
mgohain <mgohain@zaloni.com>
2016-07-07 12:41:00 +0530

    Initial commit with word count problem solution

---
+++ word-count/src/main/java/com/zaloni/mgohain/word_count/WordCount.java
@@ -0,0 +1,58 @@
+package com.zaloni.mgohain.word_count;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+/**
+ * Created by mgohain on 7/7/2016.
+ */
+public class WordCount {
+    public static void main (String [] args) {
+        String input = args[0];
+        String output = args[1];
+        SparkConf conf = new SparkConf().setAppName("Word Count");
+        JavaSparkContext javaSparkContext = new JavaSparkContext(conf);
+        //Loading input data
+        JavaRDD<String> lines = javaSparkContext.textFile(input);
+        /**
+         * MAP PHASE
+         * Splitting the lines into words
+         * FlatMapFunction<String, String>() -- here first String is the argument type and second String is the return type
+         * Each line will get split at ","
+         */
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            public Iterable<String> call(String s) throws Exception {
+                return Arrays.asList(s.split(","));
+            }
+        });
+        /**
+         * Mapping the words with count 1 eg. (word, 1)
+         * PairFunction<String, String, Integer>() -- First String is the argument type and <second(key), third(Value)> Strings are the type of the tuple to be returned
+         */
+        JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                return new Tuple2<String, Integer>(s, 1) ;
+            }
+        });
+        /**
+         * REDUCE PHASE
+         * Getting the total counts of the words eg (word,12)
+         * Function2<Integer, Integer, Integer>() -- First and second Integer are the argument types and third integer is the return type
+         */
+        JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            public Integer call(Integer a, Integer b) throws Exception {
+                return (a + b);
+            }
+        });
+        //Saving the result
+        counts.saveAsTextFile(output);
+    }
+}
